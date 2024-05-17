0a3746c739fbbcc94b19cfe8f6e6c129512a02cb
Clément Bouakil <clement.bouakil.ohmydev@gmail.com>
2024-05-17 13:30:00 +0200

    fix

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     connection: {
       connectionString: env("DATABASE_URL"),
       host: env("PGHOST", "127.0.0.1"),
-      port: env.int("PGPORT", 38815),
+      port: env.int("PGPORT", 5931),
       database: env("PGDATABASE", "railway"),
       user: env("PGUSER", "postgres"),
       password: env("PGPASSWORD", "password"),
