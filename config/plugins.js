b76938a0b2c0e26e8785bb3f36095b31153bfcd1
Clément Bouakil <clement.bouakil.ohmydev@gmail.com>
2024-05-19 17:08:36 +0200

    fix

--- config/plugins.js
+++ config/plugins.js
@@ -5,7 +5,7 @@ module.exports = () => ({
       endpoint: "/graphql",
       shadowCRUD: true,
       playgroundAlways: false,
-      depthLimit: 10,
+      depthLimit: 20,
       amountLimit: 100,
       apolloServer: {
         tracing: false,
