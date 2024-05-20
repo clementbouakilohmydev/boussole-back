0ac36a9f087e30e1955409202ec07f2a38c78edc
Clément Bouakil <clement.bouakil.ohmydev@gmail.com>
2024-05-20 11:54:58 +0200

    fix

--- src/api/collection/controllers/collection.js
+++ src/api/collection/controllers/collection.js
@@ -13,6 +13,24 @@ const collectionQuery = `
   }
 `;
 
+const collectionsQuery = `
+  query CollectionsQuery($query: String, $first: Int) {
+    collections(
+      sortKey: TITLE
+      query: $query
+      first: $first
+    ) {
+      nodes {
+        title
+        handle
+        image {
+          url
+        }
+      }
+    }
+  }
+`;
+
 const client = createStorefrontApiClient({
   storeDomain: "http://projet-boussole.myshopify.com",
   apiVersion: "2024-04",
@@ -20,6 +38,29 @@ const client = createStorefrontApiClient({
 });
 
 module.exports = createCoreController("api::collection.collection", () => ({
+  async find(ctx) {
+    const { data, meta } = await super.find(ctx);
+
+    const isMultiple = data.length > 1;
+
+    const query = data
+      .map(
+        (
+          /** @type {{ attributes: { shopifyID: any; }; }} */ c,
+          /** @type {number} */ index
+        ) =>
+          `id:${c.attributes.shopifyID}${
+            index < data.length - 1 && isMultiple ? " OR " : ""
+          }`
+      )
+      .join("");
+
+    const { data: shopify } = await client.request(collectionsQuery, {
+      variables: { first: data.length, query },
+    });
+
+    return { data, meta, shopify };
+  },
   async findOne(ctx) {
     const response = await super.findOne(ctx);
     const id = `gid://shopify/Collection/${response?.data?.attributes?.shopifyID}`;
