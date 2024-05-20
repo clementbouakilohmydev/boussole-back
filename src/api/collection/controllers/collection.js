97229f183f4be4a03ac9b54653ad735cab30eb91
Clément Bouakil <clement.bouakil.ohmydev@gmail.com>
2024-05-20 11:17:15 +0200

    fix

--- src/api/collection/controllers/collection.js
+++ src/api/collection/controllers/collection.js
@@ -1,9 +1,32 @@
-'use strict';
+const { createCoreController } = require("@strapi/strapi").factories;
+const { createStorefrontApiClient } = require("@shopify/storefront-api-client");
 
-/**
- * collection controller
- */
+const collectionQuery = `
+  query CollectionQuery($id: ID) {
+    collection(id: $id) {
+      title
+      handle
+      image {
+        url
+      }
+    }
+  }
+`;
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const client = createStorefrontApiClient({
+  storeDomain: "http://projet-boussole.myshopify.com",
+  apiVersion: "2024-04",
+  publicAccessToken: "963489a685f158c6a0e499c449321340",
+});
 
-module.exports = createCoreController('api::collection.collection');
+module.exports = createCoreController("api::collection.collection", () => ({
+  async findOne(ctx) {
+    const response = await super.findOne(ctx);
+    const id = `gid://shopify/Collection/${response?.data?.attributes?.shopifyID}`;
+    const { data } = await client.request(collectionQuery, {
+      variables: { id },
+    });
+    response.shopify = data;
+    return response;
+  },
+}));
